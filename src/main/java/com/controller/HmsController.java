c5ec1fd6d2bf1bdc03fd252ec395e03091fe0d07
pengsijie2020 <hyf52013145201314>
2020-04-26 22:46:53 +0800

    first commit two commit

---
+++ src/main/java/com/controller/HmsController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 体检管理系统
+ * @author xh
+ *
+ *
+ */
+@Controller
+@RequestMapping("hms")
+public class HmsController {
+
+
+
+
+}
